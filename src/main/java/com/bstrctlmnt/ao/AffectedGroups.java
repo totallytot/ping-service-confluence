2a57280dc3c8db6510424419c08959083b9d6646
Totally Tot <fcukthepop@gmail.com>
2018-07-06 17:56:30 +0300

    Adding AO

---
+++ src/main/java/com/bstrctlmnt/ao/AffectedGroups.java
@@ -0,0 +1,10 @@
+package com.bstrctlmnt.ao;
+
+import net.java.ao.Entity;
+
+public interface AffectedGroups extends Entity {
+
+    void setAffectedGroup(String key);
+    String getAffectedGroup();
+
+}
