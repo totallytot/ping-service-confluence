609d2c8bbb7a6d2851cdfd8661e93f8c0e99d756
Totally Tot <fcukthepop@gmail.com>
2018-08-03 14:24:31 +0300

    sql

--- src/main/resources/pages.sql
+++ src/main/resources/pages.sql
@@ -1,11 +1,11 @@
 select c.contentid, c.contenttype, sp.spacekey, c.creator, c.lastmodifier, c.lastmoddate from confluence.content c
-  inner join confluence.spaces sp
-  on sp.spaceid = c.spaceid
-  where c.contenttype = 'PAGE'
-        and c.content_status = 'current'
-        and sp.spacekey in ('TEST') --monitored space keys
-        and c.lastmoddate <  '2018-07-15 19:35:10.441' /*-- current date-timeframe*/
-        and c.creator in
+inner join confluence.spaces sp
+on sp.spaceid = c.spaceid
+where c.contenttype = 'PAGE'
+      and c.content_status = 'current'
+      and sp.spacekey in ('TEST') --monitored space keys
+      and c.lastmoddate <  '2018-07-15 19:35:10.441' /*-- current date-timeframe*/
+      and c.creator in
           (
             select distinct um.user_key from confluence.cwd_user cu
             inner join confluence.user_mapping um
